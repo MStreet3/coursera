709542d694fcb866559a75bfbc53b000194d0102
Michael Street <mi.street@gmail.com>
2019-04-05 14:58:05 -0400

    adding Header and Footer

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
 import React, {
   Component
 } from 'react';
-import logo from './logo.svg';
-import {
-  Navbar,
-  NavbarBrand
-} from 'reactstrap';
+//import logo from './logo.svg';
 import './App.css';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
 
 class App extends Component {
   render() {
-    return ( <
-      div className = "App" >
-      <
-      Navbar dark color = "primary" >
-      <
-      div className = "container" >
-      <
-      NavbarBrand href = "/" > Ristorante Con Fusion < /NavbarBrand>           < /
-      div > <
-      /Navbar>
-
-      <
-      /div>
+    return ( 
+    <div className = "App" >
+      <Header />
+      <Footer />
+    </div>
     );
   }
 }
