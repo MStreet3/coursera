688e2b58be21e40a03fc37f8cae4747ce903839e
Michael Street <mi.street@gmail.com>
2019-04-05 15:39:38 -0400

    clickable menu components

--- src/App.js
+++ src/App.js
@@ -6,13 +6,20 @@ import './App.css';
 import Header from './components/HeaderComponent';
 import Footer from './components/FooterComponent';
 import Menu from './components/MenuComponent';
+import {DISHES} from './shared/dishes';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
   render() {
     return ( 
     <div className = "App" >
       <Header />
-      <Menu />
+      <Menu dishes = {this.state.dishes} />
       <Footer />
     </div>
     );
