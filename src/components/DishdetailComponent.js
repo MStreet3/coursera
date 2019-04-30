e52bd101358574660416402fc3612650ef567cea
Michael Street <mi.street@gmail.com>
2019-04-30 16:52:28 -0400

    Assignment 3 completed

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
   Card,
   CardImg,
@@ -6,10 +6,34 @@ import {
   CardTitle,
   CardText,
   Breadcrumb,
-  BreadcrumbItem
+  BreadcrumbItem,
+  Button,
+  Row,
+  Col,
+  Label,
+  Modal,
+  ModalHeader,
+  ModalBody
 } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { LocalForm, Control, Errors } from 'react-redux-form';
 import moment from 'moment';
+
+const required = (val) => {
+  return val && val.length;
+};
+
+const maxLength = (len) => {
+  return (val) => {
+    return !val || val.length <= len;
+  };
+};
+
+const minLength = (len) => {
+  return (val) => {
+    return val && val.length >= len;
+  };
+};
 
 function RenderDish({ dish }) {
   if (dish != null) {
@@ -48,11 +72,137 @@ function RenderComments({ comments }) {
               );
             })}
           </CardText>
+          <CommentForm />
         </CardBody>
       </Card>
     );
   } else {
     return <div />;
+  }
+}
+
+class CommentForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isModalActive: false
+    };
+    this.toggleModal = this.toggleModal.bind(this);
+  }
+
+  toggleModal(e) {
+    e.preventDefault();
+    this.setState({
+      isModalActive: !this.state.isModalActive
+    });
+  }
+
+  handleSubmit(values) {
+    console.log('Current State is: ' + JSON.stringify(values));
+    alert('Current State is: ' + JSON.stringify(values));
+  }
+
+  render() {
+    return (
+      <div>
+        <Button outline onClick={this.toggleModal}>
+          <span className="fa fa-sign-in fa-lg" /> Leave a Comment
+        </Button>
+
+        <Modal isOpen={this.state.isModalActive} toggle={this.toggleModal}>
+          <ModalHeader toggle={this.toggleModal}>Submit a Comment</ModalHeader>
+          <ModalBody>
+            <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+              <Row className="form-group">
+                <Label htmlFor="rating" md={2}>
+                  Rating
+                </Label>
+                <Col md={10}>
+                  <Control.select
+                    model=".rating"
+                    id="rating"
+                    name="rating"
+                    validators={{
+                      required
+                    }}
+                  >
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                  </Control.select>
+                  <Errors
+                    className="text-danger"
+                    model=".rating"
+                    show="touched"
+                    messages={{
+                      required: 'Required.'
+                    }}
+                  />
+                </Col>
+              </Row>
+              <Row className="form-group">
+                <Label htmlFor="firstname" md={2}>
+                  Your Name
+                </Label>
+                <Col md={10}>
+                  <Control.text
+                    model=".firstname"
+                    id="firstname"
+                    name="firstname"
+                    placeholder="Your Name"
+                    className="form-control"
+                    validators={{
+                      required,
+                      minLength: minLength(3),
+                      maxLength: maxLength(15)
+                    }}
+                  />
+                  <Errors
+                    className="text-danger"
+                    model=".firstname"
+                    show="touched"
+                    messages={{
+                      required: 'Required',
+                      minLength: 'Must be greater than 2 characters',
+                      maxLength: 'Must be 15 characters or less'
+                    }}
+                    wrapper={(props) => (
+                      <ul className="list-unstyled text-danger">
+                        {props.children}
+                      </ul>
+                    )}
+                    component="li"
+                  />
+                </Col>
+              </Row>
+              <Row className="form-group">
+                <Label htmlFor="message" md={2}>
+                  Comment
+                </Label>
+                <Col md={10}>
+                  <Control.textarea
+                    model=".message"
+                    id="message"
+                    name="message"
+                    rows="6"
+                    className="form-control"
+                  />
+                </Col>
+              </Row>
+              <Row className="form-group">
+                <Col md={{ size: 10, offset: 2 }}>
+                  <Button type="submit" color="primary">
+                    Submit Comment
+                  </Button>
+                </Col>
+              </Row>
+            </LocalForm>
+          </ModalBody>
+        </Modal>
+      </div>
+    );
   }
 }
 
